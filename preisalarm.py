0318843ba326e99173f2c24594c5c7a52681a785
Quarkie1979 <peter.wolfisberg@gmail.com>
2026-07-18 10:45:50 +0200

    Update preisalarm.py

--- preisalarm.py
+++ preisalarm.py
@@ -1,6 +1,3 @@
-import sys
-
-print("=== START DES MINIMAL-TESTS ===")
-print("Hallo! Das Skript wurde erfolgreich gestartet.")
-print("=== ENDE DES MINIMAL-TESTS ===")
-sys.exit(0)
+print("TEST START")
+import requests
+print("TEST ENDE")
